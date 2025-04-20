fe75d73d841e7fb1fc9b3d62e2f2b9bcd3b0ddc5
sbutler <2097027bruh@gmail.com>
2025-04-20 18:12:16 +0000

    gsds

--- q2.py
+++ q2.py
@@ -1,3 +1,26 @@
 # all sophomore level cs majors, all freshmen not majoring in math or cs, all senior math and cs majors
 
+def mathmajors():
+    m=set()
+    num=0
+    infile=open('math.txt','r')
+    for line in infile:
+        num+=1
+        if num==1:
+            continue
+        line=line.strip().replace(","," ")
+        m.add(line)
+    return m
 
+
+def csmajors():
+    c=set()
+    num=0
+    infile=open('cs.txt','r')
+    for line in infile:
+        num+=1
+        if num==1:
+            continue
+        line=line.strip().replace(","," ")
+        c.add(line)
+    return c
