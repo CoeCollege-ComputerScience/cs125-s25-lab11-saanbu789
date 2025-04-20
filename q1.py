608a89a95356fe1b084c422f3375e483727a0b21
sbutler <2097027bruh@gmail.com>
2025-04-20 18:11:46 +0000

    question 1 done

---
+++ q1.py
@@ -0,0 +1,35 @@
+# all math-cs double majors, all majoring in math or cs, all strictly cs majors
+
+
+def mathmajors():
+    m=set()
+    num=0
+    infile=open('math.txt','r')
+    for line in infile:
+        num+=1
+        if num==1:
+            continue
+        line=line.strip().replace(","," ")
+        m.add(line)
+    return m
+
+
+def csmajors():
+    c=set()
+    num=0
+    infile=open('cs.txt','r')
+    for line in infile:
+        num+=1
+        if num==1:
+            continue
+        line=line.strip().replace(","," ")
+        c.add(line)
+    return c
+
+# print(mathmajors())
+# print(csmajors())
+m=mathmajors()
+c=csmajors()
+print(m.intersection(c))
+print(m.symmetric_difference(c))
+print(c.difference(m))
---
+++ q2.py
@@ -0,0 +1,3 @@
+# all sophomore level cs majors, all freshmen not majoring in math or cs, all senior math and cs majors
+
+
